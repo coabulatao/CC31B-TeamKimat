05e6365290f66a73525812cb40ccb36e4bf237a8
PKPanem <156826047+PKPanem@users.noreply.github.com>
2024-03-06 07:51:38 +0800

    World.cs

--- Engine/Models/World.cs
+++ Engine/Models/World.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    internal class World
-    {
+    public class World
+    {   
+        private List<Location> _locations = new List<Location>();
+        internal void AddLocation(int x, int y, string name, string desc, string imageName)
+        {
+            Location loc = new Location();
+            loc.XCoordinate = x;
+            loc.YCoordinate = y;    
+            loc.Name = name;
+            loc.Description = desc;
+            loc.ImageName = imageName;
+
+            _locations.Add(loc);
+        }
+        public Location LocationAt(int x, int y)
+        { 
+            foreach (Location loc in _locations)
+            {
+                if(loc.XCoordinate == x && loc.YCoordinate == y)
+                {
+                    return loc;
+                }
+            }
+            return null;
+        }
     }
 }
